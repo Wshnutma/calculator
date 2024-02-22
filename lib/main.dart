c919790174153cab5cfb2e87bd0084c20e40d195
Wshnutma <adityawishnutama07@gmail.com>
2024-02-23 00:00:20 +0700

    pertama

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_application_1/calculator.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: calculator(),
+    );
+  }
+}
